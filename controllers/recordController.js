ab8c31659a8f97c88ac97905537d6e3cad47491e
godfreyturqz <62551257+godfreyturqz@users.noreply.github.com>
2022-04-03 13:14:18 +0800

    update

--- controllers/recordController.js
+++ controllers/recordController.js
@@ -3,53 +3,67 @@ const cache = require('../utils/cache')
 
 const CACHE_KEY = 'RECORD'
 
-
 const asyncFunction = async (res, callback) => {
-    cache.delete(CACHE_KEY)
-    try {
-        const data = await callback()
-        console.log('record API')
-        res.status(200).json(data)
-
-    } catch (error) {
-        console.log('error in record API')
-        console.log(error)
-        res.status(400).json(error)
-    }
+	cache.delete(CACHE_KEY)
+	try {
+		const data = await callback()
+		console.log('record API')
+		res.status(200).json(data)
+	} catch (error) {
+		console.log('error in record API')
+		console.log(error)
+		res.status(400).json(error)
+	}
 }
-
 
 const get = async (req, res) => {
-    try {
-        if (!cache.isExpired(CACHE_KEY)) return res.status(200).json(cache.get(CACHE_KEY).data)
+	try {
+		if (!cache.isExpired(CACHE_KEY))
+			return res.status(200).json(cache.get(CACHE_KEY).data)
 
-        const data = await recordModel
-        .find()
-        .select({
-            uniqid: 1,
-            firstName: 1,
-            lastName: 1,
-        })
-        .lean()
-        .skip(res.startIndex)
-        .limit(res.limit)
+		const data = await recordModel
+			.find()
+			.select({
+				uid: 1,
+				firstName: 1,
+				lastName: 1,
+				email: 1,
+				contact: 1,
+				gender: 1,
+				houseNumber: 1,
+				street: 1,
+				barangay: 1,
+				city: 1,
+				province: 1,
+			})
+			.lean()
+			.skip(res.startIndex)
+			.limit(res.limit)
 
-        res.status(200).json(data)
-        cache.set(CACHE_KEY, data)
-    } catch (error) {
-        res.status(400).json(error)
-    }
+		res.status(200).json(data)
+		cache.set(CACHE_KEY, data)
+	} catch (error) {
+		res.status(400).json(error)
+	}
 }
 
-const getOne = (req, res) => asyncFunction(res, () => recordModel.findOne({uid: req.params.id}))
-const create = (req, res) => asyncFunction(res, () => recordModel.create(req.body))
-const remove = (req, res) => asyncFunction(res, () => recordModel.findOneAndRemove({uid: req.params.id}))
-const update = (req, res) => asyncFunction(res, () => recordModel.findOneAndUpdate({uid: req.params.id}, req.body, { new: true }))
+const getOne = (req, res) =>
+	asyncFunction(res, () => recordModel.findOne({ uid: req.params.id }))
+const create = (req, res) =>
+	asyncFunction(res, () => recordModel.create(req.body))
+const remove = (req, res) =>
+	asyncFunction(res, () => recordModel.findOneAndRemove({ uid: req.params.id }))
+const update = (req, res) =>
+	asyncFunction(res, () =>
+		recordModel.findOneAndUpdate({ uid: req.params.id }, req.body, {
+			new: true,
+		})
+	)
 
 module.exports = {
-    get,
-    getOne,
-    create,
-    remove,
-    update
+	get,
+	getOne,
+	create,
+	remove,
+	update,
 }
--- models/recordModel.js
+++ models/recordModel.js
@@ -1,18 +1,18 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const nameSchema = new Schema({ 
-    uid: { type: String, required: true, unique: true},
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    email: { type: String, required: true },
-    contact: { type: String, required: true },
-    gender: { type: String, required: true },
-    houseNumber: { type: String, required: true },
-    street: { type: String, required: true },
-    barangay: { type: String, required: true },
-    city: { type: String, required: true },
-    province: { type: String, required: true },
+const recordSchema = new Schema({
+	uid: { type: String, required: true, unique: true },
+	firstName: { type: String, required: true },
+	lastName: { type: String, required: true },
+	email: { type: String, required: true },
+	contact: { type: String, required: true },
+	gender: { type: String, required: true },
+	houseNumber: { type: String, required: true },
+	street: { type: String, required: true },
+	barangay: { type: String, required: true },
+	city: { type: String, required: true },
+	province: { type: String, required: true },
 })
 
-module.exports = mongoose.model('record', nameSchema)
+module.exports = mongoose.model('record', recordSchema)
