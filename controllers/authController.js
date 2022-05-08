8246308b58eacaed923fbd47dff16cac01d3eb39
godfreyturqz <62551257+godfreyturqz@users.noreply.github.com>
2022-05-08 16:33:40 +0800

    update

--- controllers/authController.js
+++ controllers/authController.js
@@ -9,121 +9,126 @@ const signupErrorHandler = require('../utils/signupErrorHandler')
 const JWT_COOKIE_NAME = 'refreshToken'
 
 //------------------------------------
+// CONFIG
+//------------------------------------
+
+const cookieConfig = {
+	httpOnly: true,
+	maxAge: 1 * 24 * 60 * 60 * 1000, // 1 day
+	sameSite: 'None',
+	secure: true, // will not work with Postman if set to false
+}
+
+//------------------------------------
 // SIGNUP
 //------------------------------------
-module.exports.signupUser = async (req,res) => {
-    try {
-        const { email, password } = req.body
-        // encrypt the password
-        const hashedPassword = await bcrypt.hash(password, 10)
+module.exports.signupUser = async (req, res) => {
+	try {
+		const { email, password } = req.body
+		// encrypt the password
+		const hashedPassword = await bcrypt.hash(password, 10)
 
-        const userData = await UserModel.create({ email, password: hashedPassword })
-        const accessToken = jwt.generateAccessToken(userData?._id)
-        const refreshToken = jwt.generateRefreshToken(userData?._id)
+		const userData = await UserModel.create({ email, password: hashedPassword })
+		const accessToken = jwt.generateAccessToken(userData?._id)
+		const refreshToken = jwt.generateRefreshToken(userData?._id)
 
-        // Saving refreshToken
-        userData.refreshToken = refreshToken
-        await userData.save()
+		// Saving refreshToken
+		userData.refreshToken = refreshToken
+		await userData.save()
 
-        res.cookie(JWT_COOKIE_NAME, refreshToken, { 
-            httpOnly: true, 
-            maxAge: 1 * 24 * 60 * 60 * 1000,
-            sameSite: 'None',
-            secure: true
-        })
+		res.cookie(JWT_COOKIE_NAME, refreshToken, cookieConfig)
 
-        res.status(201).json({ accessToken })
-
-    } catch (error) {
-        const errors = signupErrorHandler(error)
-        res.status(400).json({ message: 'user not created', errors })
-    }
+		res.status(201).json({ accessToken })
+	} catch (error) {
+		const errors = signupErrorHandler(error)
+		res.status(400).json({ message: 'user not created', errors })
+	}
 }
 
 //------------------------------------
 // SIGNIN
 //------------------------------------
-module.exports.signinUser = async (req,res) => {
-    try {
-        // check required field
-        const { email, password } = req.body
-        if(!email || !password) return res.status(400).json({ message: 'email and password are required.' })
+module.exports.signinUser = async (req, res) => {
+	try {
+		// check required field
+		const { email, password } = req.body
+		if (!email || !password)
+			return res
+				.status(400)
+				.json({ message: 'email and password are required.' })
 
-        // check existing
-        const userData = await UserModel.findOne({ email })
-        if(!userData) return res.status(404).json({ message: 'account does not exists' })
+		// check existing
+		const userData = await UserModel.findOne({ email })
+		if (!userData)
+			return res.status(404).json({ message: 'account does not exists' })
 
-        // compare password
-        const isMatch = await bcrypt.compare(password, userData?.password)
-        if (!isMatch) return res.status(401).json({ message: 'authentication error' })
+		// compare password
+		const isMatch = await bcrypt.compare(password, userData?.password)
+		if (!isMatch)
+			return res.status(401).json({ message: 'authentication error' })
 
-        // create JWTs
-        const accessToken = jwt.generateAccessToken(userData?._id)
-        const refreshToken = jwt.generateRefreshToken(userData?._id)
+		// create JWTs
+		const accessToken = jwt.generateAccessToken(userData?._id)
+		const refreshToken = jwt.generateRefreshToken(userData?._id)
 
-        // Saving refreshToken
-        userData.refreshToken = refreshToken
-        await userData.save()
+		// Saving refreshToken
+		userData.refreshToken = refreshToken
+		await userData.save()
 
-        res.cookie(JWT_COOKIE_NAME, refreshToken, { 
-            httpOnly: true,
-            maxAge: 1 * 24 * 60 * 60 * 1000, // 1 day
-            sameSite: 'None',
-            secure: true
-        })
+		res.cookie(JWT_COOKIE_NAME, refreshToken, cookieConfig)
 
-        res.status(200).json({ accessToken })
-
-    } catch (error) {
-        res.status(401).json({message: 'signin failed', error})
-    }
+		res.status(200).json({ accessToken })
+	} catch (error) {
+		res.status(401).json({ message: 'signin failed', error })
+	}
 }
 
 //------------------------------------
 // REFRESH TOKEN
 //------------------------------------
 module.exports.refreshToken = async (req, res) => {
-    try {
-        const cookies = req.cookies
-        if (!cookies[JWT_COOKIE_NAME]) return res.status(401).json({message: 'no refresh token'})
-        console.log(cookies)
+	try {
+		const cookies = req.cookies
+		if (!cookies[JWT_COOKIE_NAME])
+			return res.status(401).json({ message: 'no refresh token' })
 
-        const refreshToken = cookies[JWT_COOKIE_NAME]
-        const userData = await UserModel.findOne({ refreshToken })
-        if (!userData) return res.status(403).json({ message: 'refresh token does not exists' })
-        
-        // verify jwt 
-        const decoded = jwt.verify(refreshToken)
-        if (decoded._id !== userData._id.toString()) return res.status(403).json({ message: 'not verified' })
+		const refreshToken = cookies[JWT_COOKIE_NAME]
+		const userData = await UserModel.findOne({ refreshToken })
+		if (!userData)
+			return res.status(403).json({ message: 'refresh token does not exists' })
 
-        const accessToken = jwt.generateAccessToken(userData._id)
-        res.status(200).json({accessToken})
+		// verify jwt
+		const decoded = jwt.verify(refreshToken)
+		if (decoded._id !== userData._id.toString())
+			return res.status(403).json({ message: 'not verified' })
 
-    } catch (error) {
-        res.status(403).json({ message: 'refresh token invalid or expired' })
-    }
+		const accessToken = jwt.generateAccessToken(userData._id)
+		res.status(200).json({ accessToken })
+	} catch (error) {
+		res.status(403).json({ message: 'refresh token invalid or expired' })
+	}
 }
 
 //------------------------------------
 // SIGNOUT
 //------------------------------------
 module.exports.signoutUser = (req, res) => {
-    res.cookie(JWT_COOKIE_NAME, '', { httpOnly:true, maxAge: 1 }).send()
+	res.cookie(JWT_COOKIE_NAME, '', { httpOnly: true, maxAge: 1 }).send()
 }
-
 
 //X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X
 // FOR DEV PURPOSES
 //X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X
 
 // GET ALL USERS
-module.exports.getAllUsers = async (req,res) => {
-    const data = await UserModel.find()
-    res.json(data)
+module.exports.getAllUsers = async (req, res) => {
+	const data = await UserModel.find()
+	res.json(data)
 }
 
 // DELETE ALL USERS
 module.exports.deleteAllUsers = (req, res) => {
-    UserModel.deleteMany({}).then((res)=> res.json('success'))
-    .catch(err => res.json(err))
+	UserModel.deleteMany({})
+		.then((res) => res.json('success'))
+		.catch((err) => res.json(err))
 }
